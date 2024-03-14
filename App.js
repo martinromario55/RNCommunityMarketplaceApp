494bf1371065f3b7a95810c3fbc4255a3ec4160b
martinromario55 <martinromario55@gmail.com>
2024-03-14 05:44:31 +0300

    Clerk Authentication

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import LoginScreen from './src/screens/LoginScreen'
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo'
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white">
-      <StatusBar style="auto" />
-      <LoginScreen />
-    </View>
+    <ClerkProvider publishableKey="">
+      <View className="flex-1 bg-white">
+        <StatusBar style="auto" />
+        <SignedIn>
+          <Text>You are Signed In</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
+      </View>
+    </ClerkProvider>
   )
 }
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,7 +1,29 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useCallback } from 'react'
+import * as WebBrowser from 'expo-web-browser'
+import { useWarmUpBrowser } from '../hooks/useWarmUpBrowser'
+import { useOAuth } from '@clerk/clerk-expo'
 
+WebBrowser.maybeCompleteAuthSession()
 const LoginScreen = () => {
+  useWarmUpBrowser()
+  const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' })
+
+  const onPress = useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow()
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId })
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error('OAuth error', err)
+    }
+  }, [])
+
   return (
     <View>
       <Image
@@ -14,7 +36,7 @@ const LoginScreen = () => {
           Buy Sell Marketplace where you can sell old items and make real money
         </Text>
         <TouchableOpacity
-          onPress={() => {}}
+          onPress={onPress}
           className="p-4 bg-blue-500 mt-20 rounded-full"
         >
           <Text className="text-[18px] text-white text-center">
